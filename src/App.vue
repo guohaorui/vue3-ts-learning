7270c5098125ea7624884c2cb3a3b524c754c92b
henryguo <11612812@mail.sustech.edu.cn>
2024-01-30 01:26:41 +0800

    0130import报错解决moduleResolution:node

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
 
   <svg-icon name="lock" color="pink" width="100px" height="100px"></svg-icon>
   <Pagination />
-  <el-button type="primary" size="default" :icon="Plus">主要按钮</el-button>
-  <el-button type="success" size="small" :icon="Edit">编辑</el-button>
+  <!-- <el-button type="primary" size="default" :icon="Plus">主要按钮</el-button>
+  <el-button type="success" size="small" :icon="Edit">编辑</el-button> -->
 
   <div>
     <h1>测试sass</h1>
