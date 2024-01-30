d6e9a450b18dcefbb6e78d7df2597e0b26015db4
henryguo <11612812@mail.sustech.edu.cn>
2024-01-31 01:27:45 +0800

    0131登录页面静态搭建

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,64 @@
 <template>
-  <div>
-    <h1>我是一级路由登录</h1>
+  <div class="login-container">
+    <el-row>
+      <el-col :span="12" :xs="0">test1</el-col>
+      <el-col :span="12" :xs="24">
+        <el-form class="login-form">
+          <h1>Hello</h1>
+          <h2>欢迎来到瑞兹平台</h2>
+          <el-form-item>
+            <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password" show-password></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="login-btn" type="primary" size="default">
+              登录
+            </el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { User, Lock } from '@element-plus/icons-vue'
+import { reactive } from 'vue'
 
-<style lang="scss"></style>
+// 收集账号和密码的数据
+let loginForm = reactive({ username: 'admin', password: '111111' });
+</script>
+
+<style lang="scss">
+.login-container {
+  width: 100%;
+  height: 100%;
+  background: url('@/assets/images/background.jpg') no-repeat;
+  background-size: cover;
+
+  .login-form {
+    position: relative;
+    width: 80%;
+    top: 30vh;
+    background: url(@/assets/images/login_form.jpg) no-repeat;
+    background-size: cover;
+    padding: 40px;
+
+    h1 {
+      color: white;
+      font-size: 40px;
+    }
+
+    h2 {
+      color: white;
+      font-size: 20px;
+      margin: 20px 0
+    }
+
+    .login-btn {
+      width: 100%;
+    }
+  }
+}
+</style>
