dbd9f040b8d2c63c7ddcd2c044851efd68c28e37
henryguo <11612812@mail.sustech.edu.cn>
2024-02-01 01:38:43 +0800

    0201登录模块表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,16 +3,21 @@
     <el-row>
       <el-col :span="12" :xs="0"></el-col>
       <el-col :span="12" :xs="24">
-        <el-form class="login-form">
+        <el-form
+          class="login-form"
+          :model="loginForm"
+          :rules="rules"
+          ref="loginForms"
+        >
           <h1>Hello</h1>
           <h2>欢迎来到瑞兹平台</h2>
-          <el-form-item>
+          <el-form-item prop="username">
             <el-input
               :prefix-icon="User"
               v-model="loginForm.username"
             ></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop="password">
             <el-input
               type="password"
               :prefix-icon="Lock"
@@ -42,14 +47,43 @@ import { reactive, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import useUserStore from '@/store/modules/user'
 import { ElNotification } from 'element-plus'
+import { getTime } from '@/utils/time'
 let userStore = useUserStore()
+let loginForms = ref()
 let $router = useRouter()
 // 定义变量控制按钮加载效果
 let login_loading = ref(false)
 
 // 收集账号和密码的数据
 let loginForm = reactive({ username: 'admin', password: '111111' })
+let rules = {
+  username: [
+    // required表示这个字段必须要校验
+    { required: true, message: 'Please input username', trigger: 'blur' },
+    {
+      required: true,
+      min: 4,
+      max: 10,
+      message: 'Length of account needs to be from 4 to 10.',
+      trigger: 'blur',
+    },
+  ],
+  password: [
+    { required: true, message: 'Please input password', trigger: 'blur' },
+    {
+      required: true,
+      min: 4,
+      max: 10,
+      message: 'Length of password needs to be from 4 to 10.',
+      trigger: 'blur',
+    },
+  ],
+}
 const login = async () => {
+  // 保证所有表单项校验通过后再发起请求
+  console.log(loginForms.value)
+  let result = loginForms.value.validate()
+
   // 1. 通知仓库发登录请求
   // 2. 请求成功后展示数据
   // 3. 请求失败需要弹出登录失败信息
@@ -59,9 +93,11 @@ const login = async () => {
     login_loading.value = true
     await userStore.userLogin(loginForm)
     $router.push('/')
+    getTime()
     ElNotification({
       type: 'success',
       message: '登录成功',
+      title: `Hi, ${getTime()}好`,
     })
   } catch (error) {
     ElNotification({
